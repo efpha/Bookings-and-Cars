8c3a88210ccbaa0e04d0e9dec58b5e1aefbe9e2b
KEFFA <muthurikeffa254@gmail.com>
2024-05-24 16:31:23 +0300

    Intoduced alert to navbar.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,20 +5,24 @@ function Body(){
     return(
         <div className="container">
             <div className="card-location-desc">
-                <h1 className="location-desc">We serve you across entire USA</h1>
+                <h1 className="location-desc">Access our services from anywhere in the globe.</h1>
             </div>
             <div className="card-container">
                 <div className="card">
-                    <h2 className="assistance-description">Book a flight with us and 10% discount</h2>
-                    <button className="btn-available">Find flight</button>
+                    <h2 className="assistance-description">Book a flight with us and get 10% discount.<br/></h2>
+                    <button className="btn-available">Catch Flight</button>
                 </div>
                 <div className="card">
-                    <h2 className="assistance-description"><br/>hire</h2>
-                    <button className="btn-available">Hire Cars</button>
+                    <h2 className="assistance-description">Compare rental car deals to find the<br/>right one.</h2>
+                    <button className="btn-available">Find Rental Car</button>
                 </div>
                 <div className="card">
-                    <h2 className="assistance-description">VIP escorts for occasions such as <br/>weddings and others</h2>
-                    <button className="btn-available">Get escorted</button>
+                    <h2 className="assistance-description">Compare hotels from<br/>100s of sites.<br/></h2>
+                    <button className="btn-available">Get An Hotel</button>
+                </div>
+                <div className="card">
+                    <h2 className="assistance-description">Compare hotels from<br/>100s of sites.<br/></h2>
+                    <button className="btn-available">Get An Hotel</button>
                 </div>
             </div>            
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import home from './assets/home.png'
 import CSS from './App.css'
 import Home from './Home'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
+import { faBed } from '@fortawesome/free-solid-svg-icons';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { faUmbrellaBeach } from '@fortawesome/free-solid-svg-icons';
 
+function Navbar() {
+    const [isAlertVisible, setIsAlertVisible] = useState(true);
 
-function Navbar(){
-    return(
+    useEffect(() => {
+        // Set a timeout to hide the alert after 5 seconds
+        const timer = setTimeout(() => {
+            setIsAlertVisible(false);
+        }, 5000);
+
+        // Clear the timeout if the component is unmounted before 5 seconds
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
         <div>
+            {isAlertVisible && <div className="alert">Responsiveness underway!</div>}
             <div className="nav-bar">
                 <div className="logo"><a href={Home}>Quicklink</a></div>
-                    <div className="links-container">
-                        <ul>
-                            <li><a href={Home}>Home<FontAwesomeIcon className="homeIcon" icon="fa-solid fa-house" /></a></li>
-                            <li><a href="#">Hotels</a></li>
-                            <li><a href="#">Vacation rentals</a></li>
-                            <li><a href="#">Cars for Hire</a></li>
-                            <li><a href="#">VIP escort</a></li>
-                            <li><a href="#">Discounted Deals</a></li>
-                            {/* <li>
-                                <div className="account">
-                                    <a href="">Account</a>
-                                </div>
-                            </li> */}
-                        </ul>
-                    </div>
+                <div className="links-container">
+                    <ul>
+                        <li><a href={Home}>Home<FontAwesomeIcon className="homeIcon" icon={faHouse} /></a></li>
+                        <li><a href="#">Flights<FontAwesomeIcon className="homeIcon" icon={faPlaneDeparture} /></a></li>
+                        <li><a href="#">Stays<FontAwesomeIcon className="homeIcon" icon={faBed} /></a></li>
+                        <li><a href="#">Car Hires<FontAwesomeIcon className="homeIcon" icon={faCar} /></a></li>
+                        <li><a href="#">Packages<FontAwesomeIcon className="homeIcon" icon={faUmbrellaBeach} /></a></li>
+                        <FontAwesomeIcon icon="fa-solid fa-bed" />
+                        {/* <li>
+                            <div className="account">
+                                <a href="">Account</a>
+                            </div>
+                        </li> */}
+                    </ul>
                 </div>
             </div>
-    )
+        </div>
+    );
 }
 
 export default Navbar;
